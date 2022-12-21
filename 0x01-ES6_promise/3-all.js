704021013fbe27971a907fa8678144831d7fa6f0
abrhamgg <abrhamgg@gmail.com>
2022-12-21 12:32:30 +0300

    3-then

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,16 @@
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((body) => {
+      createUser()
+        .then((user) => {
+          const out = `${body.body} ${user.firstName} ${user.lastName}`;
+          console.log(out);
+        }).catch(() => {
+          console.log('Signup system offline');
+        });
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
 }
-
-handleProfileSignup();
