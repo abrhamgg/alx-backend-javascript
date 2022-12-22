dbb613848bb18cfdabafb202f5869c630bc4cc9c
abrhamgg <abrhamgg@gmail.com>
2022-12-22 12:44:16 +0300

    lint fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,23 @@ export default class Currency {
     this._name = name;
     this._code = code;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     this._name = value;
   }
+
   get code() {
     return this._code;
   }
+
   set code(value) {
     this._code = value;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
