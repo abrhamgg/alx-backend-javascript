24ad6b4bb1f93953f9a373f531b31942076d00a8
abrhamgg <abrhamgg@gmail.com>
2023-01-04 14:01:36 +0300

    task-4 completed

---
+++ 0x04-TypeScript/task_4/js/subjects/Subjects.ts
@@ -0,0 +1,8 @@
+namespace Subjects {
+    export class Subject {
+        teacher:Subjects.Teacher
+        setTeacher(teacher:Subjects.Teacher) {
+            this.teacher = teacher;
+        }
+    }
+}
