5da467221b0a2a82855b047b881a219c804991d3
abrhamgg <abrhamgg@gmail.com>
2023-02-21 23:21:26 +0300

    completed task-7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,23 +1,19 @@
-const express = require('express');
+const app = require('express')();
 const countStudents = require('./3-read_file_async');
 
-const app = express();
 const port = 1245;
+app.listen(port);
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen('/students', (req, res) => {
-  const data = countStudents(process.argv[2], 0);
-  data.then((success) => {
-    const output = `This is the list of our students\n${success}`;
-    res.send(output);
-  });
-});
-
-app.listen(port, () => {
-  console.log('Server has started');
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2], 0)
+    .then((success) => {
+      const out = `This is the list of our students\n${success}`;
+      res.send(out);
+    });
 });
 
 module.exports = app;
