3600f5091d864987cd5cf0f12dc9f26de420e341
abrhamgg <abrhamgg@gmail.com>
2023-02-21 22:23:20 +0300

    complete task4

---
+++ 0x05-Node_JS_basic/4-https.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = 1245;
+const app = http.createServer((_req, res) => {
+  res.end('Hello Holberton School!\n');
+});
+
+app.listen(port, () => {
+  console.log('server has started');
+});
