2d97c34e1113a1a2af71c939a65930dedfeb1045
abrhamgg <abrhamgg@gmail.com>
2023-02-24 11:45:50 +0300

    completed task-3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const Utils = require('./utils');
+const {spy} = require('sinon');
+const {expect} = require('chai')
+
+const sendPaymentRequestToApi = require('./3-payment')
+describe('sendPaymentRequestToApi', function() {
+    it('should call calculateNumber', function() {
+        const calcNumSpy = spy(Utils, 'calculateNumber');
+        const consoleSpy = spy(console, 'log');
+        const requestResponse = sendPaymentRequestToApi(100, 20);
+    
+        expect(calcNumSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
+        expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
+        calcNumSpy.restore();
+        consoleSpy.restore();
+    })
+});
